8a6c4b8b57b8545812e2b62dbc39ad1a5d8c3032
AlexeyBudugay <o.buduhai@scalr.com>
2025-05-22 14:47:14 +0300

    Change main.tf data

--- main.tf
+++ main.tf
@@ -1,5 +1,45 @@
-resource "random_pet" "run_from_vcs" {
+variable "run_id" {
+  default = "💕💕💕"
+
+}
+
+variable "sleep_time" {
+  default = 120
+}
+
+resource "random_integer" "a--__--" {
+  min = 32
+  max = 180
+
   keepers = {
-    timestamp = timestamp()
+    run_id = var.run_id
   }
+}
+
+resource "null_resource" "b__--__" {
+  triggers = {
+    run_id = var.run_id
+  }
+  provisioner "local-exec" {
+    command = "sleep ${var.sleep_time}"
+  }
+}
+
+resource "null_resource" "c_____________-_____________-__________" {
+  triggers = {
+    run_id = var.run_id
+  }
+  provisioner "local-exec" {
+    command = "env"
+  }
+}
+
+
+
+
+
+output "senc_out" {
+  value = "😁😁😁😁😁😁"
+  description = "my sensitive output"
+
 }
